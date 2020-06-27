d7f3ba80c233b70317dee6f4948088004836a733
Girma Nigusse <xgirma@users.noreply.github.com>
2020-06-27 07:54:59 -0400

    Update sandbox.spec.ts

--- spec/sandbox.spec.ts
+++ spec/sandbox.spec.ts
@@ -1,12 +1,10 @@
 import { Selector } from "testcafe";
 import * as assert from "assert";
 
-fixture`Sandbox`.page`https://e2e-boilerplate.github.io/sandbox/`;
+fixture`Sandbox`.page`https://www.google.com`;
 
 test("should be on Sandbox", async (t) => {
   const title = await Selector("title").innerText;
-  const header = await Selector("h1").innerText;
 
   assert.strictEqual(title, "Sandbox");
-  assert.strictEqual(header, "Sandbox");
 });
