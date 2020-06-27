66eea7ab4f4aad56118742ac4158a9cd86d4971e
Girma Nigusse <xgirma@users.noreply.github.com>
2020-06-27 07:55:11 -0400

    Update sandbox.spec.ts

--- spec/sandbox.spec.ts
+++ spec/sandbox.spec.ts
@@ -6,5 +6,5 @@ fixture`Sandbox`.page`https://www.google.com`;
 test("should be on Sandbox", async (t) => {
   const title = await Selector("title").innerText;
 
-  assert.strictEqual(title, "Sandbox");
+  assert.strictEqual(title, "Google");
 });
